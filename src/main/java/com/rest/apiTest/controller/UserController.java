d39ceb33b8b3d3bca266cb49b99c7a48ac2e8a28
suman-kim <zzsdsdsd@gmail.com>
2022-06-09 20:01:01 +0900

    swagger 연동

--- src/main/java/com/rest/apiTest/controller/UserController.java
+++ src/main/java/com/rest/apiTest/controller/UserController.java
@@ -4,23 +4,28 @@ package com.rest.apiTest.controller;
 import com.rest.apiTest.entity.User;
 import com.rest.apiTest.entity.UserDto;
 import com.rest.apiTest.repo.UserJpaRepo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = {"1. User"})
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/api")
 public class UserController {
     private final UserJpaRepo userJpaRepo;
 
+    @ApiOperation(value = "회원 조회", notes = "모든 회원을 조회한다.")
     @GetMapping(value = "/user")
     public List<User> findAllUser(){
 
         return userJpaRepo.findAll();
     }
 
+    @ApiOperation(value = "회원 입력", notes = "회원을 입력한다.")
     @ResponseBody
     @PostMapping(value = "/user")
     public User save(@RequestBody UserDto userdto){
